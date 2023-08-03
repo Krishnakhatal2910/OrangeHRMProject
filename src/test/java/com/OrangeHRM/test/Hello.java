cfd05ed1991e8604aa587197890a8a85e1c7ff72
Krishnakhatal2910 <141248117+Krishnakhatal2910@users.noreply.github.com>
2023-08-03 19:26:03 +0530

    Create Hello.java

---
+++ src/test/java/com/OrangeHRM/test/Hello.java
@@ -0,0 +1,13 @@
+package com.OrangeHRM.test;
+
+import org.testng.annotations.Test;
+
+
+public class Domo {
+
+	@Test
+	public void demo() {
+		System.out.println("Hey Hello Good Evening");
+	}
+
+}
